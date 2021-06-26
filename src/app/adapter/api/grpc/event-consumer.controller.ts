db330f35842bb51926b5732fc05f54ab96dd5633
DuongVu98 <duongvutung1998@gmail.com>
2021-06-27 06:15:49 +0700

    add grpc listener

---
+++ src/app/adapter/api/grpc/event-consumer.controller.ts
@@ -0,0 +1,51 @@
+import { Controller, Logger } from "@nestjs/common";
+import { GrpcMethod } from "@nestjs/microservices";
+import { Builder } from "builder-pattern";
+import { CreateProfileCommand } from "src/app/domain/commands/commands";
+import { EventConsumeGateway } from "../../controller/consume.gateway";
+
+interface UserRegisteredEventProtobuf {
+	eventId: string;
+	userId: string;
+	username: string;
+	email: string;
+	firstName: string;
+	lastName: string;
+}
+
+interface ServiceResponseProtobuf {
+	message: string;
+}
+
+const successMessage = "SUCCESS";
+const failureMessage = "FAILURE";
+
+@Controller()
+export class EventConsumerGrpc {
+	private logger = new Logger("EventConsumerGateway");
+
+	constructor(private consumeGateway: EventConsumeGateway) {}
+
+	@GrpcMethod("EventConsumerRpc", "ConsumeUserRegisteredEvent")
+	consumeUserRegisteredEvent(event: UserRegisteredEventProtobuf): Promise<ServiceResponseProtobuf> {
+		this.logger.log(`consume event: ${event.eventId}`);
+
+		const command = Builder(CreateProfileCommand)
+			.email(event.email)
+			.fullName(`${event.firstName} ${event.lastName}`)
+			.phoneNumber("")
+			.userId(event.userId)
+			.build();
+
+		// return this.consumeGateway
+		// 	.send(command)
+		// 	.then(() => {
+		// 		return { message: successMessage };
+		// 	})
+		// 	.catch(error => {
+		// 		this.logger.error(`Error during consume event: ${error}`);
+		// 		return { message: failureMessage };
+		// 	});
+        return Promise.resolve({ message: failureMessage });
+	}
+}
--- src/app/adapter/controller/consume.gateway.ts
+++ src/app/adapter/controller/consume.gateway.ts
@@ -1,19 +1,22 @@
 import { Injectable } from "@nestjs/common";
-import { BaseCommand } from "src/app/domain/commands/commands";
+import { BaseCommand, CreateProfileCommand } from "src/app/domain/commands/commands";
 import { CommandExecutor } from "src/app/usecase/command-executor/command.executor";
 import { CommandExecutorChainFactory } from "src/app/usecase/factory/command-executor-chain.factory";
 import { CommandExecutorFactory } from "src/app/usecase/factory/command-executor.factory";
 
 @Injectable()
-export class CommandGateway {
-	constructor(private commandExecutorFactory: CommandExecutorFactory, private commandExecutorChainFactory: CommandExecutorChainFactory) {}
+export class EventConsumeGateway {
+    constructor(private commandExecutorFactory: CommandExecutorFactory, private commandExecutorChainFactory: CommandExecutorChainFactory) {}
 
 	send(command: BaseCommand): Promise<any> {
 		const executor = this.getExecutorChain(command, this.commandExecutorFactory.produce(command));
 		return executor.execute(command);
 	}
 
-	private getExecutorChain(command: BaseCommand, commandExecutor: CommandExecutor): CommandExecutor {
+    private getExecutorChain(command: BaseCommand, commandExecutor: CommandExecutor): CommandExecutor {
+        if(command instanceof CreateProfileCommand) {
+            return this.commandExecutorChainFactory.produceUuidPrepairChain(commandExecutor);
+        }
 		return commandExecutor;
 	}
-}
+} 
